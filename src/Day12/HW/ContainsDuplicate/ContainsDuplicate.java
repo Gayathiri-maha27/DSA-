b10abf3a5d58f5238105235cab2567748f0231b8
Gayathiri-maha27 <gayathirimaha06@gmail.com>
2025-11-21 14:54:15 +0530

    Update ContainsDuplicate.java

--- src/Day12/HW/ContainsDuplicate/ContainsDuplicate.java
+++ src/Day12/HW/ContainsDuplicate/ContainsDuplicate.java
@@ -1,4 +1,18 @@
 package Day12.HW.ContainsDuplicate;
 
+import java.util.*;
+
 public class ContainsDuplicate {
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int n : nums) {
+            if (set.contains(n)) return true;
+            set.add(n);
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        System.out.println(containsDuplicate(nums));
+    }
 }
